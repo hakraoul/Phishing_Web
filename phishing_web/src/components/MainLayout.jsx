afe51159e434910b4d34f1e9155ff636ee99603b
Jammie779 <114967611+jammiedev779@users.noreply.github.com>
2023-10-17 22:10:35 +0700

    update

--- phishing_web/src/components/MainLayout.jsx
+++ phishing_web/src/components/MainLayout.jsx
@@ -1,19 +1,21 @@
 import {useEffect} from 'react'
 import { Outlet,useNavigate } from 'react-router-dom'
+import Navbar from './Navbar';
+import Footer from './Footer';
 
 export default function MainLayout() {
     const nav = useNavigate();
     useEffect(() => {
         nav('/sign_in');
     },[nav]);
-    
+
   return (
     <>
-        <header></header>
+        <header><Navbar/></header>
         <main>
             <Outlet/>
         </main>
-        <footer></footer>
+        <footer><Footer/></footer>
     </>
   )
 }
