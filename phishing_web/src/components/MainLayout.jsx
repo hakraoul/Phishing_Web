6b1e6ab010c613ea625cb975832fc42b761ae674
Jammie779 <114967611+jammiedev779@users.noreply.github.com>
2023-10-18 00:08:00 +0700

    update

--- phishing_web/src/components/MainLayout.jsx
+++ phishing_web/src/components/MainLayout.jsx
@@ -6,12 +6,12 @@ export default function MainLayout() {
   return (
     <>
         <header className='mt-5 mx-10 '><Navbar/></header>
-        <main className='grid place-items-center mt-14'>
-            <div className='border-solid border-2 rounded w-1/2 p-5'>
+        <main className='grid place-items-center mt-16'>
+            <div className='border-solid border-2 rounded w-2/5 p-5'>
                 <Outlet/>
             </div>
         </main>
-        <footer><Footer/></footer>
+        <footer className='mt-16'><Footer/></footer>
     </>
   )
 }
--- phishing_web/tailwind.config.js
+++ phishing_web/tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'footer-bg': '#33323b',
+      },
+    },
   },
   plugins: [],
 }
