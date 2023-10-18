4232ce5b5fe76fa2139eedc7592b293b97de35db
Jammie779 <114967611+jammiedev779@users.noreply.github.com>
2023-10-18 12:39:53 +0700

    update

--- phishing_web/src/components/Sign_In.jsx
+++ phishing_web/src/components/Sign_In.jsx
@@ -1,18 +1,24 @@
-import React from 'react'
+import { useNavigate } from "react-router-dom"
 import "../style/Sign_In.css"
 
 export default function Sign_In() {
+
+  const nav = useNavigate('');
+  const onSubmit = () => {
+    nav('/Home/form');
+  }
+
   return (
     <div>
-       <form action="">
+       <form onSubmit={onSubmit} action={onSubmit}>
             <h2>Sign In</h2>
             <p className="sub-title">Please Sign In to continue</p>
                 <div class="container">
                     <label for="uname">Username</label>
-                    <input type="text" placeholder="Username" name="uname" required/>
+                    <input type="text" placeholder="Username" required/>
 
                     <label for="psw">Password</label>
-                    <input type="password" placeholder="Password" name="psw" required/>
+                    <input type="password" placeholder="Password" required/>
                         
                     <button className="submit" type="submit">Sign In</button>
                 </div>
